1c428e3bd7e7885e3ccb69ff8beac62e64b2f3d2
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-08-26 17:35:06 +0200

    double click

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const app = Vue.createApp({
    data() {
     return {
+        showBooks: true,
         title: "FInal emperior",
         author : "kovács pista",
         age : 12
     }
    },
    methods: {
-    changeTitle(title) {
-        this.title = title
+    toggleShowBooks () {
+        this.showBooks = !this.showBooks
     }
    }
 })
