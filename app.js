916b0ac507ed6110bea6cca53a9a001fb6ebd520
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-08-26 18:35:58 +0200

    dinamic classes

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const app = Vue.createApp({
    data() {
     return {
+        url : 'https://www.google.com',
         showBooks: true,
         books :[
-            {title: "name of the wind", author: "kovács béla"},
-            {title: "name of the water", author: "kovács tamás"},
-            {title: "name of the soil", author: "kovács lackó"},
+            {title: "name of the wind", author: "kovács béla", img: "képek/barbar.png"},
+            {title: "name of the water", author: "kovács tamás", img: "képek/bob.png"},
+            {title: "name of the soil", author: "kovács lackó", img:"képek/boszorkany.png"},
         ]
     }
    },
