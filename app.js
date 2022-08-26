d23a427a1bf8e47605686c6265f127f01842a917
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-08-26 17:51:42 +0200

    outputting list

--- app.js
+++ app.js
@@ -4,12 +4,24 @@ const app = Vue.createApp({
         showBooks: true,
         title: "FInal emperior",
         author : "kovács pista",
-        age : 12
+        age : 12,
+        x: 0,
+        y:0
     }
    },
    methods: {
     toggleShowBooks () {
         this.showBooks = !this.showBooks
+    },
+    handleEvent(event, data) {
+        console.log(event, event.type)
+        if (data){
+            console.log(data)
+        }
+    },
+    handleMousemove(event){
+        this.x = event.offsetX
+        this.y = event.offsetY
     }
    }
 })
