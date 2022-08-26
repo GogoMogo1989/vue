f1631957d1b1d7a8661e6be5ebec36ed122dd116
GogoMogo1989 <kovacsfarkasdavid@gmail.com>
2022-08-26 18:44:02 +0200

    toggling favs

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const app = Vue.createApp({
         url : 'https://www.google.com',
         showBooks: true,
         books :[
-            {title: "name of the wind", author: "kovács béla", img: "képek/barbar.png"},
-            {title: "name of the water", author: "kovács tamás", img: "képek/bob.png"},
-            {title: "name of the soil", author: "kovács lackó", img:"képek/boszorkany.png"},
+            {title: "name of the wind", author: "kovács béla", img: "képek/barbar.png" ,isFav: true},
+            {title: "name of the water", author: "kovács tamás", img: "képek/bob.png" ,isFav: false},
+            {title: "name of the soil", author: "kovács lackó", img:"képek/boszorkany.png",isFav: true},
         ]
     }
    },
